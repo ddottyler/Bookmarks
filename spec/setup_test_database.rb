6c978ee3638ef4f46191288dc02e7a5d748095ac
Dan T <dp.tyler@hotmail.co.uk>
2021-04-14 17:25:23 +0100

    comments table added. Migrations updated and comments table now truncated for tests

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,5 +5,5 @@ def setup_test_database
 
   connection = PG.connect(dbname: 'manager_bookmark_test')
   
-  connection.exec("TRUNCATE bookmarks;")
+  connection.exec("TRUNCATE bookmarks, comments;")
 end
