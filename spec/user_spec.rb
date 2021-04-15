907d86f36c85c20da41e5a1eb4a01ca1ff4b239e
Dan T <dp.tyler@hotmail.co.uk>
2021-04-15 09:58:27 +0100

    user authentication added for the happy path - user gives the correct email and password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -35,3 +35,12 @@ describe '.create' do
     User.create(email: 'test@example.com', password: 'password123')
   end
 end
+
+describe '.authenticate' do
+  it 'returns a user given a correct username and password, if one exists' do
+    user = User.create(email: 'test@example.com', password: 'password123')
+    authenticated_user = User.authenticate(email: 'test@example.com', password: 'password123')
+
+    expect(authenticated_user.id).to eq user.id
+  end
+end
